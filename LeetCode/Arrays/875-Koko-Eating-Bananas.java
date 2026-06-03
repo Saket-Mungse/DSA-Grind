35d4549f028665e94ce5d66383e24351d538afa3
Saket-Mungse <2023bit023@sggs.ac.in>
2026-06-03 11:50:23 +0530

    Add: 875 - Koko Eating Bananas

---
+++ LeetCode/Arrays/875-Koko-Eating-Bananas.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        int max = -1;
+        for(int i=0;i<n;i++){
+            max = Math.max(max,piles[i]);
+        }
+
+        int left = 1;
+        int right = max;
+        int ans = -1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            int totalHoursToEatAll = canEatAll(mid,piles); 
+            if(totalHoursToEatAll<=h){
+                ans = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    private int canEatAll(int perHour, int[] piles){
+        int total = 0;
+        for(int i=0;i<piles.length;i++){
+            total += Math.ceil((double)piles[i]/(double)perHour);
+        }
+        return total;
+    }
+}
